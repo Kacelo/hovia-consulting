f8e310b23ec2641e12bdd6650a1e3b14b1ed45f1
Vernon <vernonkachelo@gmail.com>
2023-05-08 15:42:39 +0200

    removed unused code and imports

--- frontend/src/components/pages/home.jsx
+++ frontend/src/components/pages/home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import AboutUs from "../layout/about-us/about-us";
 import HoviaCarousel from "../layout/carousel/hoviaCarousel";
 import OurServices from "../layout/ourServices/ourServices";
 import Introduction from "./introduction";
@@ -8,9 +7,8 @@ function Home() {
   return (
     <div>
       <HoviaCarousel />
-
-  <Introduction/>
-    <OurServices />
+      <Introduction />
+      <OurServices />
     </div>
   );
 }
