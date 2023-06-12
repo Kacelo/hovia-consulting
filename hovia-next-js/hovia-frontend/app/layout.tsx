e811b8a2139d510190a421e4a49e50fc753bfbd8
Vernon kacelo <User@MPSWVBDT004.pupkewitz.com>
2023-06-12 22:39:50 +0200

    landing page changes done

--- hovia-next-js/hovia-frontend/app/layout.tsx
+++ hovia-next-js/hovia-frontend/app/layout.tsx
@@ -1,24 +1,22 @@
-"use client"
-import { ChakraProvider } from '@chakra-ui/react'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import '../app/globals.css';
-import { Layout } from "antd";
+"use client";
+import { ChakraProvider } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import "../app/globals.css";
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body style={{background: "#E6E6E6 !important"}}><ChakraProvider>
-        {/* <Layout style={{background: "#E6E6E6 !important"}}> */}
-        <Navbar />
-        {children}
-        {/* </Layout> */}
-       
-        <Footer />
-        </ChakraProvider></body>
+      <body style={{ backgroundColor: "#E6E6E6 !important" }}>
+        <ChakraProvider>
+          <Navbar />
+          {children}
+          <Footer />
+        </ChakraProvider>
+      </body>
     </html>
-  )
+  );
 }
